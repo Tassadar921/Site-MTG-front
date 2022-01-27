f697ccbce29487d55b70e35c4ee3c897a5819f92
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2022-01-27 09:28:14 +0100

    pop

--- front/src/app/home/home.page.ts
+++ front/src/app/home/home.page.ts
@@ -1,12 +1,91 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit{
 
+  public iname;
+  public ipassword;
+  public imail;
+  public confPassword;
+
+  private retour;
+
+  public cname;
+  public cpassword;
+
+  public output='';
+
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+  ngOnInit() {
+    this.http.post('http://loginMTG.tassadar.ovh:8080/mail', 'a').subscribe(response => {
+      console.log(response);
+    });
+  }
+
+  update=()=>{};
+
+  async signUp() {
+
+    this.output='';
+
+    if (this.ipassword && this.confPassword && this.ipassword===this.confPassword) {
+
+      this.output='';
+
+      const data = {
+        name: this.iname,
+        password: this.ipassword,
+        mail: this.imail
+      };
+
+      await this.http.post('http://loginMTG.tassadar.ovh:8080/signIn', data).subscribe(response => {
+        this.retour=response;
+        this.output=this.retour.message;
+      });
+
+    }else{
+      if(this.ipassword && this.confPassword && this.ipassword!==this.confPassword) {
+        this.output = 'Mots de passe différents';
+      }else{
+        if(!this.iname){
+          this.output='Nom d\'utilisateur requis';
+        }else{
+          if(!this.ipassword){
+            this.output='Mot de passe requis';
+          }else{
+            if(!this.confPassword){
+              this.output='Confirmation du mot de passe requise';
+            }else{
+              if(!this.imail){
+                this.output='E-mail requis';
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  async signIn(){
+    const data = {
+      name: this.cname,
+      password: this.cpassword
+    };
+    await this.http.post('http://loginMTG.tassadar.ovh:8080/login', data).pipe().subscribe(response => {
+      this.retour=response;
+      this.output=this.retour.message;
+      if(this.retour.co===true){
+        console.log(data.name);
+      }
+    });
+  }
 }
